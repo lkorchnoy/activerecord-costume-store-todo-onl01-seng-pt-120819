826d7c533fa79297e4ae63244be58dad7773f71b
Ludmila Korchnoy <lkorchnoy@yahoo.com>
2020-03-12 18:15:58 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,13 +1,13 @@
 class CreateCostumeStores < ActiveRecord::Migration[4.2]
   def change
-    create_table costume_stores do |t|
+    create_table :costume_stores do |t|
       t.string   :name 
       t.string   :location 
-      t.integer  :number of costumes 
-      t.integer  :number of employees
-      t.boolean  :still in business
-      t.datetime :opening time
-      t.datetime :closing time
+      t.integer  :number_of_costumes 
+      t.integer  :number_of_employees
+      t.boolean  :still_in_business
+      t.datetime :opening_time
+      t.datetime :closing_time
       end
   end
 end
